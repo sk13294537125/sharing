51c9f9f82cd847d189764e974c76a30342a907d9
ext.shikai1 <ext.shikai1@jd.com>
2024-09-16 01:09:05 +0800

    --

---
+++ sharing-web/src/test/java/com/sharing/cn/common/data/sql.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `tc_foreigner_reserve_order` (
+                                              `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                                              `order_id` varchar(32) DEFAULT NULL COMMENT '订单号',
+                                              `reserve_id` varchar(32) DEFAULT NULL COMMENT '预约单id（首博返回）',
+                                              `reserve_no` varchar(32) DEFAULT NULL COMMENT '预约单编号（首博返回）',
+                                              `pay_type` varchar(2) DEFAULT NULL COMMENT '支付类型：0-免费，1-待支付，2-支付成功',
+                                              `reserve_type` varchar(2) DEFAULT NULL COMMENT '预约类型：0-卡类型预约、1-证件类型预约',
+                                              `order_date` int DEFAULT NULL COMMENT '订单日期（下单）',
+                                              `order_time` varchar(20) DEFAULT NULL COMMENT '订单时间（下单）',
+                                              `order_state` varchar(2) DEFAULT NULL COMMENT '订单状态；0.待使用  1.已取消 2.已使用 3.已过期 ',
+                                              `customer_id` varchar(32) DEFAULT NULL COMMENT '用户id',
+                                              `people_num` varchar(2) DEFAULT NULL COMMENT '人数',
+                                              `mchnt_id` varchar(20) DEFAULT NULL COMMENT '公园id',
+                                              `mchnt_name` varchar(20) DEFAULT NULL COMMENT '公园名称',
+                                              `reserve_date` varchar(8) DEFAULT NULL COMMENT '预约日期,yyyyMMdd',
+                                              `reserve_time` varchar(20) DEFAULT NULL COMMENT '预约时段',
+                                              `channel_id` varchar(32) DEFAULT NULL COMMENT '渠道编号：app-eticket：APP',
+                                              `deleted` varchar(2) DEFAULT NULL COMMENT '删除状态；0初始 1删除',
+                                              `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                              `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP '修改时间',
+                                              PRIMARY KEY (`id`),
+                                              KEY `idx_customer_id` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='外籍人-预约订单表';
